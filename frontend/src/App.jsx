6e0afab25e164434fbfc7004df50773385d42c08
priya <priyaaashu4@gmail.com>
2025-03-25 00:04:32 +0530

    add routing and pages for user and captain authentication; integrate react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import Captainlogin from "./pages/Captainlogin";
+import CaptainSignup from "./pages/CaptainSignup";
 const App = () => {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/login" element={UserLogin} />
+        <Route path="/signup" element={UserSignup} />
+        <Route path="/captain-login" element={Captainlogin} />
+        <Route path="/captain-signup" element={CaptainSignup} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
